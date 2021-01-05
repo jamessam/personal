687b7b6d37778b8d8d7ae5a11b7960ce5eaee557
jamessam <jamessam@users.noreply.github.com>
2021-01-04 21:04:54 -0800

    add netlify config

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,21 @@
 import PageWrapper from '../components/PageWrapper';
 
-const NotFoundPage = () => (
-  <PageWrapper>
-    <div
-      style={{ marginTop: '100px', marginBottom: '100px', textAlign: 'center' }}
-    >
-      <h1>That page was not found.</h1>
-      <p>
-        Hey there. It looks like you're trying to access a page that doesn't
-        exist. Can you check your URL again?
-      </p>
-    </div>
-  </PageWrapper>
-);
-
-export default NotFoundPage;
+export default function NotFoundPage() {
+  return (
+    <PageWrapper>
+      <div
+        style={{
+          marginTop: '100px',
+          marginBottom: '100px',
+          textAlign: 'center',
+        }}
+      >
+        <h1>That page was not found.</h1>
+        <p>
+          Hey there. It looks like you're trying to access a page that doesn't
+          exist. Can you check your URL again?
+        </p>
+      </div>
+    </PageWrapper>
+  );
+}
