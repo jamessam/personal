7a897f25204216ac26cf57199278919b488d9ff9
jamessam <jamessam@users.noreply.github.com>
2021-01-04 20:12:59 -0800

    update BlogSummary

--- pages/blog/categories/[category].js
+++ pages/blog/categories/[category].js
@@ -1,7 +1,7 @@
-import { BlogSummary } from '../../../utils';
+import Link from 'next/link';
+import { client } from '../../../utils';
 import PageWrapper from '../../../components/PageWrapper';
 import SEO from '../../../components/SEO';
-import { client } from '../../../utils';
 
 export default function category(props) {
   const { blogs, category, categoryID } = props;
@@ -58,3 +58,40 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
+
+const BlogSummary = ({ blog }) => {
+  let urlSlug = `/blog/${blog.fields.slug}`;
+  let category = blog.fields.category;
+  let categorySlug = !category
+    ? '/'
+    : `/blog/categories/${category.fields.slug}`;
+  let categoryName = !category ? 'not categorized' : category.fields.name;
+
+  return (
+    <div>
+      <Link href={urlSlug}>
+        <a style={styles.headings}>
+          <h3 style={{ marginBottom: '0' }}>{blog.fields.title}</h3>
+          <div>
+            {blog.fields.writtenOn} | {blog.fields.shortDescription}
+          </div>
+        </a>
+      </Link>
+      <Link href={categorySlug}>
+        <a style={styles.categoryStyle}>{categoryName}</a>
+      </Link>
+      <br />
+    </div>
+  );
+};
+
+const styles = {
+  categoryStyle: {
+    fontVariantCaps: 'all-small-caps',
+    color: '#999999',
+  },
+  headings: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+};
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,7 @@
-import { BlogSummary } from '../../utils';
+import Link from 'next/link';
+import { client } from '../../utils';
 import PageWrapper from '../../components/PageWrapper';
 import SEO from '../../components/SEO';
-import { client } from '../../utils';
 
 export default function blogHome(props) {
   const { blogs } = props;
@@ -35,3 +35,40 @@ export async function getStaticProps({ params, preview = false }) {
     },
   };
 }
+
+const BlogSummary = ({ blog }) => {
+  let urlSlug = `/blog/${blog.fields.slug}`;
+  let category = blog.fields.category;
+  let categorySlug = !category
+    ? '/'
+    : `/blog/categories/${category.fields.slug}`;
+  let categoryName = !category ? 'not categorized' : category.fields.name;
+
+  return (
+    <div>
+      <Link href={urlSlug}>
+        <a style={styles.headings}>
+          <h3 style={{ marginBottom: '0' }}>{blog.fields.title}</h3>
+          <div>
+            {blog.fields.writtenOn} | {blog.fields.shortDescription}
+          </div>
+        </a>
+      </Link>
+      <Link href={categorySlug}>
+        <a style={styles.categoryStyle}>{categoryName}</a>
+      </Link>
+      <br />
+    </div>
+  );
+};
+
+const styles = {
+  categoryStyle: {
+    fontVariantCaps: 'all-small-caps',
+    color: '#999999',
+  },
+  headings: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+};
